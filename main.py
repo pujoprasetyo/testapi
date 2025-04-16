3145ce35bcf2b2d5a7e825001f1e440046c07507
Pujo Prasetyo <pujo.prasetyoaji@gmail.com>
2025-04-16 12:19:28 +0700

    updet

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from fastapi import FastAPI, HTTPException, Header
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
-
-API_KEY = "phase0h8"
 
 data = {"name":"shopping cart",
         "columns":["prod_name","price","num_items"],
@@ -11,7 +9,7 @@ data = {"name":"shopping cart",
 @app.get("/")
 def root():
     return {"message":"Welcome to Toko H8 Shopping Cart! There are some features that you can explore",
-            "menu":{1:"See shopping cart (/data)",
+            "menu":{1:"See shopping cart (/cart)",
                     2:"Add item (/add) - You may need request",
                     3:"Edit shopping cart (/edit/id)",
                     4:"Delete item from shopping cart (/del/id)"}}
@@ -21,32 +19,23 @@ def show():
     return data
 
 @app.post("/add")
-def add_item(added_item:dict, api_key: str = Header(None)):
-    if api_key is None or api_key != API_KEY:
-        raise HTTPException(status_code=401, detail="Invalid API Key. You are not allowed to add data!")
-    else:
-        id = len(data["items"].keys())+1
-        data["items"][id] = added_item
-        return f"Item successfully added into your cart with ID {id}"
+def add_item(added_item:dict):
+    id = len(data["items"].keys())+1
+    data["items"][id] = added_item
+    return f"Item successfully added into your cart with ID {id}"
 
 @app.put("/edit/{id}")
-def update_cart(id:int,updated_cart:dict, api_key: str = Header(None)):
+def update_cart(id:int,updated_cart:dict):
     if id not in data['items'].keys():
         raise HTTPException(status_code=404, detail=f"Item with ID {id} not found")
     else:
-        if api_key is None or api_key != API_KEY:
-            raise HTTPException(status_code=401, detail="Invalid API Key. You are not allowed to edit data!")
-        else:
-            data["items"][id].update(updated_cart)
-            return {"message": f"Item with ID {id} has been updated successfully."}
+        data["items"][id].update(updated_cart)
+        return {"message": f"Item with ID {id} has been updated successfully."}
 
 @app.delete("/del/{id}")
-def remove_row(id:int, api_key: str = Header(None)):
+def remove_row(id:int):
     if id not in data['items'].keys():
         raise HTTPException(status_code=404, detail=f"Item with ID {id} not found")
     else:
-        if api_key is None or api_key != API_KEY:
-            raise HTTPException(status_code=401, detail="Invalid API Key. You are not allowed to delete data!")
-        else:
-            data["items"].pop(id)
-            return {"message": f"Item with ID {id} has been deleted successfully."}
+        data["items"].pop(id)
+        return {"message": f"Item with ID {id} has been deleted successfully."}
